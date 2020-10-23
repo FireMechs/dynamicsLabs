240558078016bff033b7df08fff94349ecc623a8
EricoDeMecha <techcider4134@gmail.com>
2020-10-23 13:43:13 +0300

    error in main file fixed

--- lab1_main.m
+++ lab1_main.m
@@ -1,3 +1,21 @@
+%---------Main-------------
+function lab1_main
+    theta2 = get_precision_points(15,165,3);
+    theta4 = get_theta_4(theta2);
+    constants = compute_freudensteins_constants(theta2, theta4);
+    disp(["The constants are k1: ", constants(1)," k2: ", constants(2)," k3: ", constants(3)]);
+    [a,b,c,d] = lengths_of_links(constants);
+    disp(["The lengths of the links are crank:", a,"  coupler: ", b, " follower ", c, "fixed ", d]);
+    transmission_angles = get_transmission_angles(a,b,c,d,15,165, 5);
+    disp(["Transmission angles: ", transmission_angles]);
+    %plotting
+    figure;
+    input_angles = 15:5:165;
+    plot(input_angles, transmission_angles, 'r');
+    xlabel("Input angles");
+    ylabel("Output angles");
+    title("Input angles vs transmission angles");
+end
 % get the precision points using chebyshev's spacing.
 function precision_points = get_precision_points(lower_limit, upper_limit, no_of_points)
     precision_points = zeros(1,no_of_points);
@@ -53,19 +71,3 @@ function  [a,b,c,d] = lengths_of_links(constants)
     c = d / constants(2);
     b = sqrt((a * a) + (c * c) + (d * d) - (2 * a * c * constants(3)));
 end
-%---------Main-------------
-theta2 = get_precision_points(15,165,3);
-theta4 = get_theta_4(theta2);
-constants = compute_freudensteins_constants(theta2, theta4);
-disp(["The constants are k1: ", constants(1)," k2: ", constants(2)," k3: ", constants(3)]);
-[a,b,c,d] = lengths_of_links(constants);
-disp(["The lengths of the links are crank:", a,"  coupler: ", b, " follower ", c, "fixed ", d]);
-transmission_angles = get_transmission_angles(a,b,c,d,15,165, 5);
-disp(["Transmission angles: ", transmission_angles]);
-%plotting
-figure;
-input_angles = 15:5:165;
-plot(input_angles, transmission_angles, 'r');
-xlabel("Input angles");
-ylabel("Output angles");
-title("Input angles vs transmission angles");
